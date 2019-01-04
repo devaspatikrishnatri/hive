d765dc1873b30b825fbc9edd7ab93e63b99111af
Szehon Ho <szehon.apache@gmail.com>
2019-01-04 11:45:03 +0100

    CDPD-15734 HIVE-21033 : Forgetting to close operation cuts off any more HiveServer2 output (Szehon, reviewed by Aihua)
    (cherry picked from commit 16d39c6abbc7ad6d2fc6732eadf04b09eaaf8809)
    Change-Id: I6d58333d791ffce37381069a48c673b64257de10

--- cli/src/test/org/apache/hadoop/hive/cli/TestCliDriverMethods.java
+++ cli/src/test/org/apache/hadoop/hive/cli/TestCliDriverMethods.java
@@ -47,6 +47,7 @@ import jline.console.completer.Completer;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.common.io.SessionStream;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.metastore.api.FieldSchema;
@@ -123,10 +124,10 @@ public class TestCliDriverMethods {
 
     // Capture stdout and stderr
     ByteArrayOutputStream dataOut = new ByteArrayOutputStream();
-    PrintStream out = new PrintStream(dataOut);
+    SessionStream out = new SessionStream(dataOut);
     System.setOut(out);
     ByteArrayOutputStream dataErr = new ByteArrayOutputStream();
-    PrintStream err = new PrintStream(dataErr);
+    SessionStream err = new SessionStream(dataErr);
     System.setErr(err);
 
     CliSessionState ss = new CliSessionState(new HiveConf());
@@ -187,7 +188,7 @@ public class TestCliDriverMethods {
     when(proc.getSchema()).thenReturn(mockSchema);
 
     CliSessionState mockSS = mock(CliSessionState.class);
-    PrintStream mockOut = mock(PrintStream.class);
+    SessionStream mockOut = mock(SessionStream.class);
 
     mockSS.out = mockOut;
 
@@ -260,8 +261,8 @@ public class TestCliDriverMethods {
   public void testQuit() throws Exception {
 
     CliSessionState ss = new CliSessionState(new HiveConf());
-    ss.err = System.err;
-    ss.out = System.out;
+    ss.err = new SessionStream(System.err);
+    ss.out = new SessionStream(System.out);
 
     try {
       CliSessionState.start(ss);
@@ -292,7 +293,7 @@ public class TestCliDriverMethods {
     CliSessionState sessinState = new CliSessionState(new HiveConf());
     CliSessionState.start(sessinState);
     ByteArrayOutputStream data = new ByteArrayOutputStream();
-    sessinState.err = new PrintStream(data);
+    sessinState.err = new SessionStream(data);
     sessinState.database = "database";
     CliDriver driver = new CliDriver();
 
@@ -328,8 +329,8 @@ public class TestCliDriverMethods {
 
     ByteArrayOutputStream data = new ByteArrayOutputStream();
 
-    sessionState.err = new PrintStream(data);
-    sessionState.out = System.out;
+    sessionState.err = new SessionStream(data);
+    sessionState.out = new SessionStream(System.out);
     try {
       CliSessionState.start(sessionState);
       CliDriver cliDriver = new CliDriver();
