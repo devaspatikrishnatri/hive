da76a083ff5233f6ea6e364d58512407b8cd95fb
Ashutosh Chauhan <hashutosh@apache.org>
2020-05-15 23:02:35 -0700

    CDPD-15730 HIVE-23292 : Reduce PartitionDesc payload in MapWork (Rajesh Balamohan, Ramesh Kumar, Ashutosh Chauhan via Ashutosh Chauhan)
    Change-Id: I1ab48a5a925c3ab75659c4e5a499ab8ff3ca2c49

--- ql/src/java/org/apache/hadoop/hive/ql/plan/PartitionDesc.java
+++ ql/src/java/org/apache/hadoop/hive/ql/plan/PartitionDesc.java
@@ -73,8 +73,6 @@ public class PartitionDesc implements Serializable, Cloneable {
   public PartitionDesc() {
   }
 
-  private final static org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(PartitionDesc.class);
-
   public PartitionDesc(final TableDesc table, final LinkedHashMap<String, String> partSpec) {
     this.tableDesc = table;
     setPartSpec(partSpec);
@@ -223,6 +221,7 @@ public class PartitionDesc implements Serializable, Cloneable {
   }
 
   public void setProperties(final Properties properties) {
+    properties.remove("columns.comments");
     if (properties instanceof CopyOnFirstWriteProperties) {
       this.properties = properties;
     } else {
