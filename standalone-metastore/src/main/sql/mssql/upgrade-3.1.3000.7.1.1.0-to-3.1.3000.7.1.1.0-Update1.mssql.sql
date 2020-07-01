48ee573dccaf47df78fca2c6edc33baa7ea91a0f
Naveen Gangam <ngangam@cloudera.com>
2020-07-01 17:24:50 -0400

    DWX-4548: Update schema version to 7.1.1.0-Update1
    Change-Id: If34d870800f3858ab658cab4ec0edcb531b972cf

---
+++ standalone-metastore/src/main/sql/mssql/upgrade-3.1.3000.7.1.1.0-to-3.1.3000.7.1.1.0-Update1.mssql.sql
@@ -0,0 +1,3 @@
+-- These lines need to be last.  Insert any changes above.
+UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.1.1.0-Update1', VERSION_COMMENT='Hive release version 3.1.3000.7.1.1.0' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.1.1.0 to 3.1.3000.7.1.1.0-Update1' AS MESSAGE;
---
+++ standalone-metastore/src/main/sql/mysql/upgrade-3.1.3000.7.1.1.0-to-3.1.3000.7.1.1.0-Update1.mysql.sql
@@ -0,0 +1,3 @@
+
+UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.1.1.0-Update1', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.1.1.0' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.1.1.0 to 3.1.3000.7.1.1.0-Update1';
