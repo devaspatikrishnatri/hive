b33dc994864a494971fd51e032373e5ee556ea19
Ayush Saxena <ayushsaxena@apache.org>
2021-03-18 15:41:16 +0530

    CDPD-26266 HIVE-24853: HMS leaks queries in case of timeout. (#2044) (Ayush Saxena reviewed by Zoltan Haindrich)
    (cherry picked from commit eed78dfdcb6dfc2de400397a60de12e6f62b96e2)
    Change-Id: I18c2624aa6eaac3eeb3d8c419e243858e9b76f99

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveAlterHandler.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveAlterHandler.java
@@ -337,7 +337,7 @@ public class HiveAlterHandler implements AlterHandler {
                   partBatch, newt.getWriteId(), writeIdList);
             }
           }
-
+          Deadline.checkTimeout();
           for (Entry<Partition, ColumnStatistics> partColStats : columnStatsNeedUpdated.entries()) {
             ColumnStatistics newPartColStats = partColStats.getValue();
             newPartColStats.getStatsDesc().setDbName(newDbName);
@@ -380,6 +380,7 @@ public class HiveAlterHandler implements AlterHandler {
                 List<ColumnStatistics> colStats = updateOrGetPartitionColumnStats(msdb, catName, dbname, name,
                     part.getValues(), oldCols, oldt, part, null, null);
                 assert (colStats.isEmpty());
+                Deadline.checkTimeout();
                 if (cascade) {
                   msdb.alterPartition(
                     catName, dbname, name, part.getValues(), part, writeIdList);
@@ -554,6 +555,7 @@ public class HiveAlterHandler implements AlterHandler {
           updateOrGetPartitionColumnStats(msdb, catName, dbname, name, new_part.getValues(),
               oldPart.getSd().getCols(), tbl, new_part, null, null);
         }
+        Deadline.checkTimeout();
         msdb.alterPartition(
             catName, dbname, name, new_part.getValues(), new_part, validWriteIds);
         if (transactionalListeners != null && !transactionalListeners.isEmpty()) {
@@ -1032,8 +1034,8 @@ public class HiveAlterHandler implements AlterHandler {
           }
         }
       }
-
       if (doAlterTable) {
+        Deadline.checkTimeout();
         // Change to new table and append stats for the new table
         msdb.alterTable(catName, dbName, tableName, newTable, validWriteIds);
         if (updateColumnStats) {
@@ -1101,6 +1103,7 @@ public class HiveAlterHandler implements AlterHandler {
                 break;
               }
             }
+            Deadline.checkTimeout();
             if (found) {
               if (rename) {
                 if (updateColumnStats) {
