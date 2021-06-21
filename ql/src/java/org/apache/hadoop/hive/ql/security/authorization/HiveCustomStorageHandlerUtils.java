72f562b72c529e47bccfcbebfd3b307ae780968a
saihemanth <saihemanth@cloudera.com>
2021-06-21 15:24:33 -0700

    CDPD-22643: CDPD-27085: Ignore the COLUMN_STATS_ACCURATE property of table for Tables based on storage handlers
    Change-Id: I0d277cbb49a0fca3b7345b091f410d42f85dbdf7
    (cherry picked from commit 28a89641bd05d06ef7c25a752935b31c50b525c7)

--- ql/src/java/org/apache/hadoop/hive/ql/security/authorization/HiveCustomStorageHandlerUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/security/authorization/HiveCustomStorageHandlerUtils.java
@@ -19,14 +19,16 @@ package org.apache.hadoop.hive.ql.security.authorization;
 
 import java.util.Map;
 import org.apache.hadoop.hive.serde.serdeConstants;
+import org.apache.hadoop.hive.common.StatsSetupConst;
 
 public class HiveCustomStorageHandlerUtils {
 
     public static String getTablePropsForCustomStorageHandler(Map<String, String> tableProperties){
         StringBuilder properties = new StringBuilder();
         for(Map.Entry<String,String> serdeMap : tableProperties.entrySet()){
-            if(!serdeMap.getKey().equalsIgnoreCase(serdeConstants.SERIALIZATION_FORMAT)) {
-                properties.append(serdeMap.getValue().replaceAll("\\s",""));
+            if(!serdeMap.getKey().equalsIgnoreCase(serdeConstants.SERIALIZATION_FORMAT) &&
+                    !serdeMap.getKey().equalsIgnoreCase(StatsSetupConst.COLUMN_STATS_ACCURATE)) {
+                properties.append(serdeMap.getValue().replaceAll("\\s","").replaceAll("\"",""));
                 properties.append("/");
             }
         }
