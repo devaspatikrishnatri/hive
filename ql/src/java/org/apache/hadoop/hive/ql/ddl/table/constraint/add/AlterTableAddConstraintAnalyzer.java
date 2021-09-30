4c80b3cffde9c988d836a89e47ae227dce131ef0
saihemanth <saihemanth@cloudera.com>
2021-09-29 18:32:13 -0700

    CDPD-29773: HIVE-25545: ADD/DROP constraint events on table should create authorizable events in HS2
    Change-Id: I6b9e421c6038999d356d63902258821c5625ae08

--- ql/src/java/org/apache/hadoop/hive/ql/ddl/table/constraint/add/AlterTableAddConstraintAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/ddl/table/constraint/add/AlterTableAddConstraintAnalyzer.java
@@ -35,6 +35,8 @@ import org.apache.hadoop.hive.ql.ddl.table.AbstractAlterTableAnalyzer;
 import org.apache.hadoop.hive.ql.ddl.table.constraint.Constraints;
 import org.apache.hadoop.hive.ql.ddl.table.constraint.ConstraintsUtils;
 import org.apache.hadoop.hive.ql.exec.TaskFactory;
+import org.apache.hadoop.hive.ql.io.AcidUtils;
+import org.apache.hadoop.hive.ql.metadata.Table;
 import org.apache.hadoop.hive.ql.parse.ASTNode;
 import org.apache.hadoop.hive.ql.parse.HiveParser;
 import org.apache.hadoop.hive.ql.parse.SemanticException;
@@ -80,6 +82,12 @@ public class AlterTableAddConstraintAnalyzer extends AbstractAlterTableAnalyzer 
     Constraints constraints = new Constraints(primaryKeys, foreignKeys, null, uniqueConstraints, null,
         checkConstraints);
     AlterTableAddConstraintDesc desc = new AlterTableAddConstraintDesc(tableName, null, constraints);
+
+    Table tbl = getTable(tableName);
+    if(AcidUtils.isTransactionalTable(tbl)) {
+      setAcidDdlDesc(desc);
+    }
+    addInputsOutputsAlterTable(tableName, partitionSpec, desc, desc.getType(), false);
     rootTasks.add(TaskFactory.get(new DDLWork(getInputs(), getOutputs(), desc)));
   }
 }
--- ql/src/java/org/apache/hadoop/hive/ql/hooks/WriteEntity.java
+++ ql/src/java/org/apache/hadoop/hive/ql/hooks/WriteEntity.java
@@ -218,6 +218,7 @@ public class WriteEntity extends Entity implements Serializable {
     case RENAME:
     case TRUNCATE:
     case MERGEFILES:
+    case ADD_CONSTRAINT:
     case DROP_CONSTRAINT:
     case OWNER:
       return WriteType.DDL_EXCLUSIVE;
