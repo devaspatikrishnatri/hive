f51be478af67a8c68e012a6ffffcdb40ccedd1d8
Yu-Wen Lai <yu-wen.lai@cloudera.com>
2022-03-16 11:49:41 -0700

    CDPD-35942: HIVE-26041: Fix wrong type supplied for getLatestCommittedCompaction
    We should use preparedStatement.setLong() to set correct type for CC_ID.
    
    Change-Id: I9ce11d9c7d8397d1069e9536d0bf9f1fe6d5fa7f

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/txn/TxnHandler.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/txn/TxnHandler.java
@@ -3447,12 +3447,14 @@ abstract class TxnHandler implements TxnStore, TxnStore.MutexAPI {
         }
         if (rqst.isSetLastCompactionId()) {
           sb.append(" AND \"CC_ID\" > ?");
-          params.add(String.valueOf(rqst.getLastCompactionId()));
         }
         sb.append(" ORDER BY \"CC_ID\" DESC");
 
         pst = sqlGenerator.prepareStmtWithParameters(dbConn, sb.toString(), params);
-        LOG.debug("Going to execute query <" + sb.toString() + ">");
+        if (rqst.isSetLastCompactionId()) {
+          pst.setLong(params.size() + 1, rqst.getLastCompactionId());
+        }
+        LOG.debug("Going to execute query <{}>", sb);
         rs = pst.executeQuery();
         Set<String> partitionSet = new HashSet<>();
         while (rs.next()) {
@@ -3472,7 +3474,7 @@ abstract class TxnHandler implements TxnStore, TxnStore.MutexAPI {
           }
         }
       } catch (SQLException e) {
-        LOG.error("Unable to execute query " + e.getMessage());
+        LOG.error("Unable to execute query", e);
         checkRetryable(dbConn, e, "getLatestCommittedCompactionInfo");
       } finally {
         LOG.debug("Going to rollback");
