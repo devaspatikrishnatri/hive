fb87a0e96bc90283ba35c6ea90ab3effa1cf10f4
Denys Kuzmenko <dkuzmenko@cloudera.com>
2022-07-22 09:43:09 +0200

    CDPD-43307: HIVE-26416: AcidUtils.isRawFormatFile() throws InvalidProtocolBufferException for non-ORC file (Denys Kuzmenko, reviewed by Ayush Saxena)
    Closes #3460
    
    Change-Id: Ifae720e5d1acd9b9e7aa5acfdbfd9e59969a0250
    (cherry picked from commit c1cff2db9b5cf1eb42aad891722042e00b57623b)

--- ql/src/test/org/apache/hadoop/hive/ql/TestTxnCommands.java
+++ ql/src/test/org/apache/hadoop/hive/ql/TestTxnCommands.java
@@ -1324,4 +1324,29 @@ public class TestTxnCommands extends TxnCommandsBaseForTests {
 
     hiveConf.setBoolVar(HiveConf.ConfVars.HIVE_WRITE_ACID_VERSION_FILE, originalEnableVersionFile);
   }
+
+  @Test
+  public void testIsRawFormatFile() throws Exception {
+    dropTable(new String[]{"file_formats"});
+
+    runStatementOnDriver("CREATE TABLE `file_formats`(`id` int, `name` string) " +
+            " ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe' " +
+            "WITH SERDEPROPERTIES ( " +
+            " 'field.delim'='|', " +
+            " 'line.delim'='\n'," +
+            " 'serialization.format'='|')  " +
+            "STORED AS " +
+            " INPUTFORMAT " +
+            "   'org.apache.hadoop.mapred.TextInputFormat' " +
+            " OUTPUTFORMAT " +
+            "   'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat' " +
+            "TBLPROPERTIES ( " +
+            " 'transactional'='true'," +
+            " 'transactional_properties'='insert_only')");
+
+    runStatementOnDriver("insert into file_formats (id, name) values (1, 'Avro'),(2, 'Parquet'),(3, 'ORC')");
+
+    List<String> res = runStatementOnDriver("select * from file_formats");
+    Assert.assertEquals(3, res.size());
+  }
 }
