3837b3906458df98193ee1f251d1e7802b1b0b68
Ayush Saxena <ayushsaxena@apache.org>
2023-09-22 14:59:06 +0530

    CDPD-62554: HIVE-21100: Allow flattening of table subdirectories resulted when using TEZ engine and UNION clause. (#4730). (Ayush Saxena, reviewed by Laszlo Bodor)
    (cherry picked from commit e5a7ce2f091da1f8a324da6e489cda59b9e4bfc6)
    Change-Id: If55e6c05f0d742437c643efa4109ddfacd9febfe

--- ql/src/java/org/apache/hadoop/hive/ql/exec/MoveTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/MoveTask.java
@@ -21,7 +21,9 @@ package org.apache.hadoop.hive.ql.exec;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.hive.common.FileUtils;
 import org.apache.hadoop.hive.common.HiveStatsUtils;
 import org.apache.hadoop.hive.conf.HiveConf;
@@ -74,6 +76,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -95,6 +98,40 @@ public class MoveTask extends Task<MoveWork> implements Serializable {
 
   public boolean canExecuteInParallel(){
     return false;
+  }
+
+  public void flattenUnionSubdirectories(Path sourcePath) throws HiveException {
+    try {
+      FileSystem fs = sourcePath.getFileSystem(conf);
+      LOG.info("Checking {} for subdirectories to flatten", sourcePath);
+      Set<Path> unionSubdirs = new HashSet<>();
+      if (fs.exists(sourcePath)) {
+        RemoteIterator<LocatedFileStatus> i = fs.listFiles(sourcePath, true);
+        String prefix = AbstractFileMergeOperator.UNION_SUDBIR_PREFIX;
+        while (i.hasNext()) {
+          Path path = i.next().getPath();
+          Path parent = path.getParent();
+          if (parent.getName().startsWith(prefix)) {
+            // We do rename by including the name of parent directory into the filename so that there are no clashes
+            // when we move the files to the parent directory. Ex. HIVE_UNION_SUBDIR_1/000000_0 -> 1_000000_0
+            String parentOfParent = parent.getParent().toString();
+            String parentNameSuffix = parent.getName().substring(prefix.length());
+
+            fs.rename(path, new Path(parentOfParent + "/" + parentNameSuffix + "_" + path.getName()));
+
+            unionSubdirs.add(parent);
+          }
+        }
+
+        // remove the empty union subdirectories
+        for (Path path : unionSubdirs) {
+          LOG.info("This subdirectory has been flattened: " + path.toString());
+          fs.delete(path, true);
+        }
+      }
+    } catch (Exception e) {
+      throw new HiveException("Unable to flatten " + sourcePath, e);
+    }
   }
 
   private void moveFile(Path sourcePath, Path targetPath, boolean isDfsDir)
@@ -310,6 +347,9 @@ public class MoveTask extends Task<MoveWork> implements Serializable {
 
   @Override
   public int execute() {
+    boolean shouldFlattenUnionSubdirectories =
+        HiveConf.getBoolVar(conf, HiveConf.ConfVars.HIVE_TEZ_UNION_FLATTEN_SUBDIRECTORIES);
+
     if (Utilities.FILE_OP_LOGGER.isTraceEnabled()) {
       Utilities.FILE_OP_LOGGER.trace("Executing MoveWork " + System.identityHashCode(work)
         + " with " + work.getLoadFileWork() + "; " + work.getLoadTableWork() + "; "
@@ -333,6 +373,9 @@ public class MoveTask extends Task<MoveWork> implements Serializable {
           Utilities.FILE_OP_LOGGER.debug("MoveTask not moving " + sourcePath);
         } else {
           Utilities.FILE_OP_LOGGER.debug("MoveTask moving " + sourcePath + " to " + targetPath);
+          if (shouldFlattenUnionSubdirectories) {
+            flattenUnionSubdirectories(sourcePath);
+          }
           if(lfd.getWriteType() == AcidUtils.Operation.INSERT) {
             //'targetPath' is table root of un-partitioned table or partition
             //'sourcePath' result of 'select ...' part of CTAS statement
@@ -406,6 +449,9 @@ public class MoveTask extends Task<MoveWork> implements Serializable {
         boolean isFullAcidOp = work.getLoadTableWork().getWriteType() != AcidUtils.Operation.NOT_ACID
             && !tbd.isMmTable(); //it seems that LoadTableDesc has Operation.INSERT only for CTAS...
 
+        if (shouldFlattenUnionSubdirectories) {
+          flattenUnionSubdirectories(tbd.getSourcePath());
+        }
         // Create a data container
         DataContainer dc = null;
         if (tbd.getPartitionSpec().size() == 0) {
